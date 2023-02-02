4396b6ca9ee9fc0575d0913482186e5bb720e2ed
Lígia Sell <ligia.sell@hotmail.com>
2023-02-02 17:44:35 -0300

    Add Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { NextPage, InferGetStaticPropsType } from "next";
+
+import BlogCard from "../components/BlogCard";
+import { PostApiResponse } from "../utils/types";
+import { readPostInfo } from "../lib/helper";
+
+export const getStaticProps = async () => {
+  const postInfo: PostApiResponse = readPostInfo();
+
+  return {
+    props: { posts: postInfo },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Blogs: NextPage<Props> = ({ posts }): JSX.Element => {
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <div className="max-w-3xl mx-auto p-5 space-y-5">
+      {posts?.map((post) => (
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          description={post.meta}
+          slug={post.slug}
+        />
+      ))}
+    </div>
   );
-}
+};
+
+export default Blogs;
